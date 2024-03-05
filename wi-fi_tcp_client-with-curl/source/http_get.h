3eb91ddcc6e6714d5fbeabd3fb199cae6cf5fb36
GSaumya0497 <131981780+GSaumya0497@users.noreply.github.com>
2024-03-05 17:05:49 +0530

    Add files via upload

---
+++ wi-fi_tcp_client-with-curl/source/http_get.h
@@ -0,0 +1,17 @@
+#ifndef HTTP_GET_H_
+#define HTTP_GET_H_
+
+//#define URL  "https://httpbin.org/get"
+
+//#define URL  "https://httpbin.org/put"
+
+//#define URL  "https://httpbin.org/post"
+
+//#define URL  "https://httpbin.org/patch"
+
+//#define URL  "https://httpbin.org/delete"
+//#define URL "https://academics.mnnit.ac.in/new/"
+#define URL "https://curl.se/"
+
+void http_get();
+#endif
